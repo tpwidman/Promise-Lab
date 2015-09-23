4ae0259441bdbe18bccc39ee64b97a27bed251c3
tpwidman <tpwidman@gmail.com>
2015-09-23 10:13:25 -0500

    whew fixed

---
+++ Templates/UsePromises/usePromiseTemplate.js
@@ -0,0 +1,12 @@
+function mapAsync(iterator, obj, context) {
+	
+	
+};
+function mapAsyncInOrder(iterator, array, context) {
+	
+	
+};
+function mapAsyncInDescendingOrder(iterator, array, context) {
+	
+	
+};
